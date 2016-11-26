27262709126ab1bf453969ff21c332e589817312
Jeffrey Bauer <Jeffrey@Jeffreys-Air.attlocal.net>
2016-11-26 12:21:47 -0500

    Twitter & OMDB

--- keys.js
+++ keys.js
@@ -2,7 +2,7 @@ console.log('this is loaded');
 
 exports.twitterKeys = {
   consumer_key: 'dGo6BsjEqAXEsFrEcM3WsztAF',
-  consumer_secret: 'oZeQGlXBwIGu8X64LK8jiv1Od7L2vLaJZYYuoGl89Ev29FEc5q,
-  access_token_key: '	2207277643-ukEccM8566r4SKUwLQNJP6qGoTZqwfvfwCEHlwu',
+  consumer_secret: 'oZeQGlXBwIGu8X64LK8jiv1Od7L2vLaJZYYuoGl89Ev29FEc5q',
+  access_token_key: '2207277643-ukEccM8566r4SKUwLQNJP6qGoTZqwfvfwCEHlwu',
   access_token_secret: 'GUOfPLzMt6SdfRkMDnZaSFvyIgrdrXkYGTgeFMuDFudxy',
 }
--- liri.js
+++ liri.js
@@ -1,12 +1,12 @@
-var keys = requier ('keys.js');
+var keys = requier ('./keys.js');
 var twitter = require('twitter');
 var client = new twitter(keys.twitterKeys);
 var spotify = require('spotify');
-var request = requier('request')
+var request = require('request');
 
-var liri {
+var liri = {
 
-    twitterAPI: {
+    twitterApi: {
 
        getRecentTweets: function() {
            var params = { screen_name: "JeffreyB05"}
@@ -22,13 +22,43 @@ var liri {
            }
        } 
     },
-}
+
+    omdbApi: {
+
+        getMovieInfo: function (movieName) {
+            if (!movieName) {
+                movieName = "Mr. Nobody"
+            }
+
+            var apiUrl = "http://www.omdbapi.com/?t=" + movieName +  "&y=&plot=short&r=json";
+
+            request(apiUrl, this.omdbResponse)
+        },
+
+        omdbResponse: function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var parsedBody = JSON.parse(body);
+                console.log(parsedBody);
+                console.log("Movie Title: " + parsedBody.Title);
+                console.log("Release Year: " + parsedBody.Year);
+                console.log("IMDB Rating: " + parsedBody.imdbRating);
+                console.log("Produced in: " + parsedBody.Country);
+                console.log("Language: " + parsedBody.Language);
+                console.log("Plot: " + parsedBody.Plot);
+                console.log("Actors: " + parsedBody.Actors);
+        }
+    },
+},
+};
 
 switch (process.argv[2]) {
     case "my-tweets":
-    liri.twitterAPI.getRecentTweets();
+    liri.twitterApi.getRecentTweets();
     break;
-}
+    case "movie-this":
+    liri.omdbApi.getMovieInfo(process.argv[3]);
+    break;
+};
 
 
 
